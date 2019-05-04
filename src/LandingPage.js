e3e027f6a2fca8e3912d5cafb5b3d64c1b5a7448
telaak <teemu@laaksonen.me>
2019-05-04 20:21:16 +0300

    use react-router instead of window.history

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,47 +1,39 @@
 import React, { Component } from 'react';
 import Lottie from 'react-lottie';
-import animationData from './animations/Hud.json'
-import { withRouter } from "react-router-dom";
+import animationData from './animations/Hud.json';
+import { withRouter, Link } from 'react-router-dom';
 
 class LandingPage extends Component {
-
-  constructor(props) {
-    super(props);
-    this.animationClick = this.animationClick.bind(this)
-  }
-
-  //Uses react-router v4 withRouter and history.push to navigate to the wanted location
-  animationClick() {
-    this.props.history.push("/Store")
-  }
   componentWillUnmount() {
-    console.log('animation component unmounted')
+    console.log('animation component unmounted');
   }
 
   render() {
-
     const animationWrapper = {
       width: '700px'
-    }
+    };
 
     const defaultOptions = {
       loop: true,
-      autoplay: true, 
+      autoplay: true,
       animationData: animationData,
       rendererSettings: {
         preserveAspectRatio: 'xMidYMid slice'
       }
-    }
-    
+    };
 
-   return <div className="d-flex flex-column justify-content-center mt-5 pt-5">
-              <div className="p-2 d-flex row justify-content-center">
-                <div style={animationWrapper} className="animationPic" onClick={this.animationClick}>
-                  <Lottie options={defaultOptions}/>
-                </div>
-              </div>
-              <button>stop</button>
-          </div>
+    return (
+      <div className="d-flex flex-column justify-content-center mt-5 pt-5">
+        <div className="p-2 d-flex row justify-content-center">
+          <Link to="/Store">
+            <div style={animationWrapper} className="animationPic">
+              <Lottie options={defaultOptions} />
+            </div>
+          </Link>
+        </div>
+        <button>stop</button>
+      </div>
+    );
   }
 }
 
